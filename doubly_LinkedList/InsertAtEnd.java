d122584d38fdce00d6206db3b4c77579c2f92e17
Praveen jha <Praveenjhafbg54321@gmail.com>
2022-10-21 15:23:55 +0530

    Insert at End

---
+++ doubly_LinkedList/InsertAtEnd.java
@@ -0,0 +1,47 @@
+package doubly_LinkedList;
+
+public class InsertAtEnd {
+	
+	static class Node{
+		int data;
+		Node next;
+		Node prev;
+		
+		Node(int x){
+			data = x;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Node head = new Node(3);
+		addLast(head,4);
+		addLast(head,5);
+		addLast(head,6);
+		addLast(head,7);
+		iterate(head);
+
+	}
+	static Node addLast(Node head, int data) {
+		Node temp = new Node(data);
+		Node current = head;
+		if(head == null) {
+			return temp;
+		}
+		while(current.next != null) {
+			current = current.next;
+		}
+		current.next = temp;
+		temp.prev = current;
+		return head;
+	}
+	
+	static void iterate(Node head) {
+		Node current = head;
+		while(current != null){
+			System.out.print(current.data+" ");
+			current = current.next;
+		}
+	}
+
+}
