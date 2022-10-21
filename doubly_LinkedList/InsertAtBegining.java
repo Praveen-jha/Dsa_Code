0c17eba235fea43a494be852ccd13e1683ec84c3
Praveen jha <Praveenjhafbg54321@gmail.com>
2022-10-21 15:04:42 +0530

    Insert at Begin

--- doubly_LinkedList/InsertAtBegining.java
+++ doubly_LinkedList/InsertAtBegining.java
@@ -1,10 +1,44 @@
 package doubly_LinkedList;
 
 public class InsertAtBegining {
+	
+	static class Node{
+		int data;
+		Node next;
+		Node prev;
+		
+		Node(int x){
+			data = x;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Node head = new Node(2);
+		iterate(head);
+		System.out.println();
+		head = addBegin(head,3);
+		head = addBegin(head,4);
+		head = addBegin(head,5);
+		iterate(head);
 
 	}
+	static Node addBegin(Node head, int key) {
+		Node temp = new Node(key);
+		
+		temp.next = head;
+		if(head != null) {
+			head.prev = temp;
+		}
+		return temp;
+	}
+	static void iterate(Node head) {
+		Node current = head;
+		while(current != null) {
+			System.out.print(current.data+" ");
+			current = current.next;
+		}
+	}
+	
 
 }
